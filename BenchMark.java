36e2d4bc696d024c1b441e8f34734725619dc586
gharkamal <grewalh7@yahoo.com>
2015-12-28 19:10:12 -0800

    Update BenchMark.java

--- BenchMark.java
+++ BenchMark.java
@@ -17,27 +17,26 @@ public class BenchMark {
 		DataCounter<String> d = null;
 		if (args.length < 3) {
 
-			System.err.println("For checking WordCount's performance: java BenchMark [ -b | -a | -h ] [ -frequency | -num_unique ] <filename>");
-			System.err.println("For checking Correlator's performance: java BenchMark [ -b | -a | -h ] [ -frequency | -num_unique ] <filename>");
-
-
-			System.exit(1);
+		System.err.println("For checking WordCount's performance: java BenchMark [ -b | -a | -h ] [ -frequency | -num_unique ] <filename>");
+		System.err.println("For checking Correlator's performance: java BenchMark [ -b | -a | -h ] [ -frequency | -num_unique ] <filename>");
+		System.exit(1);
 		}
 		if (args[0].equals("-b"))
 		{
-			d = new BinarySearchTree<String>();
-			dType = "BST";
+		d = new BinarySearchTree<String>();
+		dType = "BST";
 		}
+		
 		else if (args[0].equals("-a"))
 		{
-			d = new AVLTree<String>();
-			dType = "AVLTree";
+		d = new AVLTree<String>();
+		dType = "AVLTree";
 		}
+		
 		else if (args[0].equals("-h"))
-
 		{
-			d = new HashTable();
-			dType = "HashTable";
+		d = new HashTable();
+		dType = "HashTable";
 		}
 		
 		checkPerformanceWordCount(d, args[1], args[2]);
@@ -71,20 +70,19 @@ public class BenchMark {
 	 * this method computes the running time of correlator  class . but we had to comment this part out to remove the error.
 	 * We can still run this method by passing "java BenchMark [-a|-h|-b] textfile1 textFile2" in the command line
 	 */
-	/**
-	 * public static void checkPerformanceCorrelator( DataCounter<String> d, DataCounter<String> d1, String file1, String file2 ){ 
+ public static void checkPerformanceCorrelator( DataCounter<String> d, DataCounter<String> d1, String file1, String file2 ){ 
         long timeSum=0, s1, e1;
-                // to calculate the running time for correlator class   
+              // to calculate the running time for correlator class   
         		s1 = System.nanoTime();
         		Correlator.correlate(d, d1, file1, file2 );
         		e1 = System.nanoTime();
         		timeSum = (e1- s1);
                 
-                // to Calculate the memory usage of the method
+         // to Calculate the memory usage of the method
                 Runtime runtime = Runtime.getRuntime();
                 runtime.gc();
                 long memory = runtime.totalMemory() - runtime.freeMemory();
                 System.out.println("Totally memory used in bytes for correlating :" + memory);
                 System.out.println("Time taken for correlating two files : "+ timeSum / 1000000000.0);
-	}*/
+	}
 }
