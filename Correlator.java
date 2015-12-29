fb2a73e244ae03632bd83c562c44f538d4fb2e5a
gharkamal <grewalh7@yahoo.com>
2015-12-28 19:16:09 -0800

    Update Correlator.java

--- Correlator.java
+++ Correlator.java
@@ -3,18 +3,19 @@
  * @author Gurpreet Singh
  */
 public class Correlator {
-	private static final double FREQ_MIN = 0.0001;
+    private static final double FREQ_MIN = 0.0001;
     private static final double FREQ_MAX = 0.01;
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static void main(String[] args)
 	{
 		if(args.length != (3))
 		{
-			 System.err.println("java Correlator [-b | -a | -h] <String File1> <String File2");
-			 System.exit(1);
+			System.err.println("java Correlator [-b | -a | -h] <String File1> <String File2");
+			System.exit(1);
 		}
 			DataCounter<String> d1 = null;
 			DataCounter<String> d2 = null;
+		
 		if(args[0].equals("-b"))
 		{
 			d1 = new BinarySearchTree();
@@ -30,10 +31,9 @@ public class Correlator {
 			d1 = new HashTable();
 			d2 = new HashTable();
 		}
-		
-		correlate(d1, d2, args[1], args[2] );
-		
+		correlate(d1, d2, args[1], args[2]);
 	}
+	
 	/**
 	 * Method to correlate the two files
 	 * @param d1 data structure type 1
@@ -70,10 +70,11 @@ public class Correlator {
              }
      }
      
-     System.out.println("The correlation between " + a + " and " + b + " is: " + sum );
+	 System.out.println("The correlation between " + a + " and " + b + " is: " + sum );
 
 	}
-	/**
+	
+     /**
      * Helper method to calculate the total number of words in a file 
      * @param file the file to be checked words for
      * @param d the data structure to be used
@@ -83,23 +84,17 @@ public class Correlator {
 	private static int size( String file, DataCounter d ) {
 		int totalWords = 0;
 		try {
-
 			FileWordReader reader = new FileWordReader(file);
-
 			String word = reader.nextWord();
-
 			while (word != null) {
-
 				d.incCount(word);
 				totalWords++;
 				word = reader.nextWord();
-
 			}
 		} catch (Throwable error) {
 			System.err.println("Error processing \n" + file + error);
 			System.exit(1);
 		}
 		return totalWords;
-
 	}
 }
